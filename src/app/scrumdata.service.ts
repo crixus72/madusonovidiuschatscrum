b58f55e032fd96338c84fa5fbce1919bafa6da70
Madu Chika <madusonovidius@gmail.com>
2020-06-23 17:09:45 +0100

    Latest working files

---
+++ src/app/scrumdata.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Scrumuser} from './scrumuser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScrumdataService {
+
+  constructor(private _http: HttpClient) {}
+  _url = 'https://liveapi.chatscrum.com/scrum/api/scrumusers/'
+
+  public httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  }
+
+  signup(user: Scrumuser) {
+    return this._http.post<any>(this._url, {'email': user['email'], 'full_name': user['fullname'],
+    'password': user['password'], 'usertype': user['usertype'], 'projname': user['projname']}, this.httpOptions);
+  }
+}
---
+++ src/app/scrumuser.ts
@@ -0,0 +1,9 @@
+export class Scrumuser {
+  constructor(
+    public email: string,
+    public fullname: string,
+    public password: string,
+    public usertype: string,
+    public projname: string,
+    ){}
+}
