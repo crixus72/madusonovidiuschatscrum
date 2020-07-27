5c21a43e8aee03e6470c1bd492faa713fb073fae
Madu Chika <madusonovidius@gmail.com>
2020-07-27 19:11:05 +0100

    Latest working files

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -15,9 +15,9 @@ export class ChatComponent implements OnInit {
     this.websocketConnection = new WebSocket(this.WS_URL);
     console.log('connect', this.websocketConnection);
   }
-  WS_URL: string ="wss://83ozfd8bq1.execute-api.us-east-1.amazonaws.com/dev"
+  WS_URL: string ="wss://hk98yb4zrh.execute-api.us-east-2.amazonaws.com/teststage"
   websocketConnection: WebSocket;
-  messages: Array<object> =[]
+  messages: Array<object> = []
   chat_text: string;
   logincred: any;
   messageFromServer: string;
@@ -27,25 +27,23 @@ export class ChatComponent implements OnInit {
   scrumLoginUserModel = new Scrumuser('', '', '', '', '');
 
   ngOnInit() {
-    this.websocketConnection.onopen = (event) => {
-      const context = {action: 'getRecentMessages'}
-      this.websocketConnection.send(JSON.stringify(context));
-    }
+    this.websocketConnection.onopen = (event) => {const context = {action: 'getRecentMessages'};
+                                                  this.websocketConnection.send(JSON.stringify(context));
+    };
     this.websocketConnection.onmessage = (event) => {
-      let data = JSON.parse(event.data)
-      console.log("message things" + data)
+      let data = JSON.parse(event.data);
+      console.log("message things" + data);
       if (data['messages'] !== undefined){
         data['messages'].forEach((message) => {
-          console.log("new =" + JSON.stringify(message))
-          this.messages.push(message)
-
-        })
-        console.log("Array "+ this.messages)
-        this.messages.forEach((message) =>{
-          console.log("allmessages " + message )
-        })
+          console.log("new =" + JSON.stringify(message));
+          this.messages.push(message);
+        });
+        console.log("Array "+ this.messages);
+        this.messages.forEach((message) => {
+          console.log("allmessages " + message );
+        });
       }
-    }
+    };
 
     // this.websocketConnection.onopen = (event)=>{
     //   const context = {action:"getRecentMessages"}
@@ -67,26 +65,26 @@ export class ChatComponent implements OnInit {
   getUsername(){
     this.logincred = JSON.parse(localStorage.getItem('Authobj'));
     // console.log("The User =" + this.logincred.name)
-    return this.logincred.name
+    return this.logincred.name;
   }
 
   getCurrentTime(){
     return new Date().toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
   }
 
-  sendMessage(){
+  sendMessage(chat_text){
     // const chat_text = this.scrumLoginUserModel.project_name;
     // let chat_text = document.getElementById("chat_text").textContent
-    console.log(this.chat_text)
+    console.log(this.chat_text);
     if(this.chat_text !== ""){
-      const context = {"action": 'sendMessage',
-       "username":this.getUsername(),
-       "content":this.chat_text,
-        "timestamp":this.getCurrentTime()};
+      const context = {action: 'sendMessage',
+       username: this.getUsername(),
+       content: this.chat_text,
+        timestamp: this.getCurrentTime()};
 
       this.websocketConnection.send(JSON.stringify(context))
-      console.log(context)
-      this.chat_text = ''
+      console.log(context);
+      this.chat_text = '';
       // this.messages.unshift(context)
       // this.scrumLoginUserModel.project_name = '';
       // window.setInterval(function () {
