2ae0519885750b87d9537c47bc42198fc4c93321
Madu Chika <madusonovidius@gmail.com>
2020-07-28 11:06:38 +0100

    Latest working files

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -12,10 +12,10 @@ import { ActivatedRoute, Router  } from '@angular/router';
 export class ChatComponent implements OnInit {
 
   constructor( private _route: ActivatedRoute,  private _router: Router, private wsService: WebsocketService) {
-    this.websocketConnection = new WebSocket(this.WS_URL);
+    this.websocketConnection = new WebSocket('wss://terzi305yg.execute-api.us-east-2.amazonaws.com/test');
     console.log('connect', this.websocketConnection);
   }
-  WS_URL: string ="wss://hk98yb4zrh.execute-api.us-east-2.amazonaws.com/teststage"
+  WS_URL: string ="wss://terzi305yg.execute-api.us-east-2.amazonaws.com/test"
   websocketConnection: WebSocket;
   messages: Array<object> = []
   chat_text: string;
